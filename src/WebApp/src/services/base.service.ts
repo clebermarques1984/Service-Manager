066bd5e90cb5d2b09a10374484a2a522768f9293
clebermarques1984 <clebermarques1984@gmail.com>
2018-06-10 01:49:23 -0300

    fix BaseService to show 'Server error' message

--- src/WebApp/src/services/base.service.ts
+++ src/WebApp/src/services/base.service.ts
@@ -4,7 +4,7 @@ export abstract class BaseService {
   protected readonly api = 'http://localhost:5000/api';
 
   protected handleError(error: any) {
-    const applicationError = error.headers['Application-Error'];
+    const applicationError = (error && error.headers['Application-Error']);
 
     if (applicationError) {
       return Observable.throw(applicationError);
@@ -12,7 +12,7 @@ export abstract class BaseService {
 
     let modelStateErrors: any = '';
 
-    if (error.data) {
+    if (error && error.data) {
       for (const key in error.data) {
         if (error.data[key]) {
           modelStateErrors += error.data[key] + '\n';
